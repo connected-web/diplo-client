9f2d8df8d9c01320d9451d4d1c48fdfded22257d
John Beech <johnbeech@users.noreply.github.com>
2020-03-28 23:49:50 +0000

    Add diplo assets to client (#1)
    Co-authored-by: Markavian <github@mkv25.net>

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Diplo Client',
+  description: 'Manage your project from here.',
+  themeConfig: {
+    logo: '/assets/diplo-icon.png',
+    nav: [
+      { text: 'Home', link: '/' }
+    ]
+  }
+}
